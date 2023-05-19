20979af0312dfad4cf03d690664a27a9ee448ea2
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-05-19 17:34:19 +0700

    case 2

--- LingkedListOOP/LingkedListOOP.cpp
+++ LingkedListOOP/LingkedListOOP.cpp
@@ -147,3 +147,20 @@ int main()
 
 		}
 		break;
+
+		case '2':
+		{
+			if (mhs.ListEmpty())
+			{
+				cout << endl << "List Kosong" << endl;
+				break;
+			}
+			cout << endl << "\nMasukkan no mahasiswa yang akan dihapus : ";
+			cin >> nim;
+			if (mhs.delNode(nim) == false)
+				cout << endl << "Data tidak ditemukan " << endl;
+			else
+				cout << endl << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus";
+
+		}
+		break;
